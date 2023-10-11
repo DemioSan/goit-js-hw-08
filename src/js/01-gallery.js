941e9efe37f1bfda329bf0d1359d593832a46b9e
DemioSan <132845079+DemioSan@users.noreply.github.com>
2023-10-11 16:55:48 +0200

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItem({ preview, original, description }) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = preview;
+  galleryImage.alt = description;
+  galleryImage.setAttribute('data-source', original);
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItem);
+galleryList.append(...galleryItemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+galleryList.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName === 'IMG') {
+    lightbox.open();
+  }
+});
