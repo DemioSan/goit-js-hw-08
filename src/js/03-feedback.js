26efcd80ba0d49c8861808f1c0e13ad7bc4213d3
DemioSan <132845079+DemioSan@users.noreply.github.com>
2023-10-11 17:59:08 +0200

    03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+const storageKey = 'feedback-form-state';
+
+const saveToLocalStorage = () => {
+  const state = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(state));
+};
+
+const restoreFromLocalStorage = () => {
+  const savedState = localStorage.getItem(storageKey);
+  if (savedState) {
+    const state = JSON.parse(savedState);
+    emailInput.value = state.email;
+    messageTextarea.value = state.message;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', restoreFromLocalStorage);
+
+emailInput.addEventListener('input', throttle(saveToLocalStorage, 500));
+messageTextarea.addEventListener('input', throttle(saveToLocalStorage, 500));
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const state = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(state);
+  localStorage.removeItem(storageKey);
+});
